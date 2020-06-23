6f198e620b25fba4b0591a289aa1263f9c6751da
German <germandolia@gmail.com>
2020-06-23 17:26:42 +0300

    Updates

---
+++ precache-manifest.568690d9d657b845287fdb6a9947754a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06970e33a3f143cf2f68eaa98f2b2f4f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "74cc4768fab5abcf4163",
+    "url": "./static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "8be6a1410b7fb5fcc929",
+    "url": "./static/css/main.5b5eb71e.chunk.css"
+  },
+  {
+    "revision": "74cc4768fab5abcf4163",
+    "url": "./static/js/2.a151ed51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a151ed51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be6a1410b7fb5fcc929",
+    "url": "./static/js/main.2946f0c7.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.568690d9d657b845287fdb6a9947754a.js"
 );
 
 self.addEventListener('message', (event) => {
